936d570bd5bbb1c388cb987526f1f110c6bb15c3
relipa-banv <banv@relipasoft.com>
2022-10-14 16:53:45 +0700

    flashlight app

--- App.js
+++ App.js
@@ -1,85 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button,
+  Alert,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Torch from 'react-native-torch';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isTorchOn, setIsTorchOn] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const delay = delayInms => {
+    return new Promise(resolve => setTimeout(resolve, delayInms));
   };
 
+  const handlePress = () => {
+    setIsTorchOn(!isTorchOn);
+  };
+
+  useEffect(() => {
+    const t = async () => {
+      console.log('99', isTorchOn);
+
+      while (isTorchOn) {
+        console.log(isTorchOn);
+        Torch.switchState(true);
+        await delay(3000);
+        Torch.switchState(false);
+      }
+    };
+    t();
+  }, [isTorchOn]);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>999 App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Button title="Press me 123" onPress={() => handlePress()} />
         </View>
       </ScrollView>
     </SafeAreaView>
